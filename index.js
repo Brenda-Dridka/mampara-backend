5d25e1e10a6a017627897e8307edc9b37becd5d4
Brenda-Dridka <ruiz83636@gmail.com>
2024-01-10 16:20:32 -0600

    cambios para vercel

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000;
 /* app.listen(port); */
 
 /* console.log("LISTO EN EL PUESTO ${port}"); */
-app.listen(port, () => {
+/* app.listen(port, () => {
   console.log("Port ==> ", port);
-});
+}); */
+app.listen(port);
+
+console.log("LISTO EN EL PUESTO ${port}");
