6cce927f4e0a1a48c0437a0830773ef2881b316a
Brenda-Dridka <ruiz83636@gmail.com>
2024-01-10 18:23:30 -0600

    implementacion

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const app = require("./src/app");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+const routerApi = require("./src/routes");
 
 const port = process.env.PORT || 3000;
 
-/* app.listen(port); */
+app.use(cors());
+app.use(express.json());
 
-/* console.log("LISTO EN EL PUESTO ${port}"); */
-/* app.listen(port, () => {
+app.get("/", (req, res) => {
+  res.send("Backend con NodeJS - Express + CRUD API REST + MySQL");
+});
+
+routerApi(app);
+
+app.listen(port, () => {
   console.log("Port ==> ", port);
-}); */
-app.listen(port);
-
-console.log("LISTO EN EL PUESTO, ${port} ");
+});
