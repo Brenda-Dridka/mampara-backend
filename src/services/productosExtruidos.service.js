b1582990841b672d1ce9e88a06db80ad9343f0f9
Brenda-Dridka <ruiz83636@gmail.com>
2024-01-26 12:40:19 -0600

    producto extruido

--- src/services/productosExtruidos.service.js
+++ src/services/productosExtruidos.service.js
@@ -1,4 +1,8 @@
-const { models } = require("../libs/sequelize");
+const { models, sequelize } = require("../libs/sequelize");
+const {
+  ProductoExtruidoSchema,
+  ProductoExtruido,
+} = require("../db/models/productosExtruidos.model");
 
 class ProductosExtruidosService {
   constructor() {}
@@ -28,6 +32,46 @@ class ProductosExtruidosService {
     const model = await this.findOne(id);
     await model.destroy();
     return { deleted: true };
+  }
+
+  async deleteMany() {
+    try {
+      // Asegúrate de tener el modelo definido correctamente en el servicio
+      if (!models.ProductoExtruido) {
+        throw new Error(
+          "El modelo no está inicializado correctamente en el servicio."
+        );
+      }
+
+      // Elimina todos los registros de la tabla
+      await models.ProductoExtruido.destroy({ where: {} });
+
+      console.log("Eliminación masiva exitosa");
+    } catch (error) {
+      console.error("Error al realizar la eliminación masiva:", error);
+      throw error;
+    }
+  }
+
+  async createMany(productoExtruido) {
+    try {
+      // Inicia la transacción
+      await sequelize.transaction(async (t) => {
+        // Guarda cada etiqueta en la transacción
+        await Promise.all(
+          productoExtruido.map(async (productoExtruido) => {
+            await models.ProductoExtruido.create(productoExtruido, {
+              transaction: t,
+            });
+          })
+        );
+      });
+
+      console.log("Guardado masivo exitoso");
+    } catch (error) {
+      console.error("Error al realizar el guardado masivo:", error);
+      throw error;
+    }
   }
 }
 
